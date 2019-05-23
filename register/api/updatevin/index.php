80de11a94e3c9b55db59ecd52f1f2b6f04168037
autobookspace <sebitestb@gmail.com>
2019-05-23 22:16:07 +0300

    modify the query a little bit; database is not updated

--- register/api/updatevin/index.php
+++ register/api/updatevin/index.php
@@ -1,10 +1,10 @@
 <?php 
 
-	if (isset($_GET['id'])) {$id=$_GET['id'];} else {$id='';}
-	if (isset($_GET['vin'])) {$vin=$_GET['vin'];} else {$vin='';}
+	if (isset($_POST['id'])) {$id=$_POST['id'];} else {$id='';}
+	if (isset($_POST['vin'])) {$vin=$_POST['vin'];} else {$vin='';}
    require_once('dbconnect.php');
    
-   $sql = "UPDATE autobook_vin SET vin='$vin' WHERE id='$id'";
+   $sql = "UPDATE autobook_vin SET vin='".$vin."' WHERE id='".$id."'";
    $result = $con -> query($sql);
     echo '{"msg":"Multumim. Datele sunt complete acum."}';
 	 
