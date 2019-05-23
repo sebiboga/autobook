727bf7c70cd4ef76372839818bb9b88e3589d2e3
sebastianboga <sebastian.boga@endava.com>
2019-05-23 11:25:21 +0300

    customize the confirm page

--- confirm/index.php
+++ confirm/index.php
@@ -1,11 +1,74 @@
-<?php 
-if (isset($_GET['id']) && ($_GET['id']!='')) {
-	$id = $_GET['id'];
-   require_once('dbconnect.php');
- 	 $sql="UPDATE autobook_link SET valid='1' WHERE id='$id'";
-	 $result = $con -> query($sql);
-	  echo "Felicitari! Acum sunteti membru autobook SPACE ; Verifica beneficiile ;)";
-}
 
- 
-?>
+<!DOCTYPE html>
+<html  >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.10.1, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.10.1, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo4.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  <title>multumesc</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+  <section class="cid-rrhgL5Sq9S mbr-fullscreen mbr-parallax-background" id="header2-0">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.5; background-color: rgb(35, 35, 35);"></div>
+
+    <div class="container align-center">
+        <div class="row justify-content-md-center">
+            <div class="mbr-white col-md-10">
+			<?php 
+				if (isset($_GET['id']) && ($_GET['id']!='')) {
+					$id = $_GET['id'];
+				   require_once('dbconnect.php');
+					 $sql="UPDATE autobook_link SET valid='1' WHERE id='$id'";
+					 $result = $con -> query($sql);
+				?>
+			
+                <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1">
+                    FELICITARI!</h1>
+                
+                <p class="mbr-text pb-3 mbr-fonts-style display-5">
+                    Acum sunteti membru autobook.space&nbsp;<br>Verifica Beneficiile</p>
+                <div class="mbr-section-btn"><a class="btn btn-md btn-secondary display-4" href="https://autobook.space">ACASA</a>
+                    <a class="btn btn-md btn-white-outline display-4" href="https://autobook.space/beneficii">BENEFICII</a></div>
+              <?php } 	else {  ?>
+				  
+				  <p class="mbr-text pb-3 mbr-fonts-style display-5">
+                    Nu reusim sa te identificam. Poate ai uitat sa te inregistrezi.
+					<a href="https://autobook.space/register/"></a>
+				  
+			<?php  }?>
+			</div>
+        </div>
+    </div>
+    
+</section>
+
+
+  <section class="engine"><a href="https://mobirise.info/w">free html5 templates</a></section><script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/parallax/jarallax.min.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  
+  
+</body>
+</html>
