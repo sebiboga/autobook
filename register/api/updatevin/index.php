efa7dcd68caf5181e453a8dd5612b370cf4f7db0
sebastianboga <sebastian.boga@endava.com>
2019-05-24 17:37:01 +0300

    if the VIN is previously added and now the user enter an empty VIN , then we don't update database. we keep the old VIN

--- register/api/updatevin/index.php
+++ register/api/updatevin/index.php
@@ -1,11 +1,12 @@
 <?php 
 
 	if (isset($_POST['id'])) {$id=$_POST['id'];} else {$id='';}
-	if (isset($_POST['vin'])) {$vin=$_POST['vin'];} else {$vin='';}
+	if (isset($_POST['vin'])&&(isset($_POST['vin'])!='')) {$vin=$_POST['vin'];
    require_once('dbconnect.php');
    
    $sql = "UPDATE autobook_vin SET vin='".$vin."' WHERE id='".$id."'";
    $result = $con -> query($sql);
     echo '{"msg":"Multumim. Datele sunt complete acum."}';
+	} 
 	 
 ?>
