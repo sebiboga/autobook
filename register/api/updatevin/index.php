d6c24be6d971be2f115f39baac7a9aaf26748875
sebastianboga <sebastian.boga@endava.com>
2019-05-24 18:14:14 +0300

    we test also if the VIN contains special characters other than a-zA-Z0-9

--- register/api/updatevin/index.php
+++ register/api/updatevin/index.php
@@ -1,12 +1,11 @@
 <?php 
 
 	if (isset($_POST['id'])) {$id=$_POST['id'];} else {$id='';}
-	if (isset($_POST['vin'])&&(trim($_POST['vin'])!='')&&(strlen($_POST['vin'])==17)) {$vin = strtoupper($_POST['vin']);
+	if (isset($_POST['vin'])&&(trim($_POST['vin'])!='')&&(strlen($_POST['vin'])==17)&&(ctype_alnum($_POST['vin']))) {$vin = strtoupper($_POST['vin']);
    require_once('dbconnect.php');
    
    $sql = "UPDATE autobook_vin SET vin='".$vin."' WHERE id='".$id."'";
    $result = $con -> query($sql);
     echo '{"msg":"Multumim. Datele sunt complete acum."}';
 	} 
-	 
 ?>
