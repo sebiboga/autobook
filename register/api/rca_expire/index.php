47b1e711a36f547b8e7c1ea235dccd3744211b6d
sebastianboga <sebastian.boga@endava.com>
2019-07-25 17:48:16 +0300

    rca_expire controller

---
+++ register/api/rca_expire/index.php
@@ -0,0 +1,53 @@
+<?php 
+function GUID() {
+    if (function_exists('com_create_guid') === true)
+    {return trim(com_create_guid(), '{}');
+    }
+    return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
+}
+
+	$id=GUID();
+   require_once('dbconnect.php');
+   
+   $sql = "SELECT rn, rca,datediff(rca,now()) as rcadiff FROM `autobook_vin` WHERE (rca IS NOT NULL) and (year(rca)>'2000') and datediff(rca,now())<=7";
+   $result = $con -> query($sql);
+   
+    if ($result->num_rows > 0) {
+		 while ($rws=$result->fetch_assoc()){
+			  
+			  $auto    	= $rws['rn'];
+			  $rca_date = $rws['rca'];
+			  $days		= $rws['rcadiff'];
+			  $text = "Pentru autovehicolul ".$auto." RCA ";
+			  if ($days=='0') {$text.= " expira azi ".$rca_date;}
+			  if ($days <0) {$text.=" a expirat de ".abs($days)." zile";}
+			  if ($days >0) {$text .= "va expira in ".$days." zile";}
+			  
+			$sql_phone = "
+			SELECT u.tel,l.id FROM `autobook_vin` v 
+			INNER JOIN autobook_link l ON l.idvin=v.id
+			INNER JOIN autobook_user u ON u.id = l.iduser
+			WHERE rn='$auto'
+			";  
+			  $result_phone = $con -> query($sql_phone);
+			  if ($result_phone->num_rows > 0) {
+			    while ($phones=$result_phone->fetch_assoc()){
+					 $phone 	= $phones['tel'];
+					 $authkey 	= $phones['id'];
+					 $timestamp	= date("Y-m-d H:i:s");
+					     $sql_insert="
+						 INSERT INTO autobook_smssend(id,phone,text,sent,command,ckey,timestamp) VALUES ('$id','$phone','$text','0','rca_expired','$authkey','$timestamp')";
+					     $result_insert = $con -> query($sql_insert);
+															}
+												}	
+			  
+		                                       }
+	
+	    
+			
+		                        }
+	 
+   
+    
+	 
+?>
