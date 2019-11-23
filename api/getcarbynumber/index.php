93de4f4cf07fa2b63db4c955d6b064599e25e21b
sebastianboga <sebastian.boga@endava.com>
2019-11-23 09:32:42 +0200

    changing getcarbynumber to point to new database/tables

--- api/getcarbynumber/index.php
+++ api/getcarbynumber/index.php
@@ -2,7 +2,7 @@
  $rn='';
  if (isset($_GET['number'])) {$rn = $_GET['number']; $rn = str_replace("-","",$rn); $rn = str_replace(" ","",$rn); $rn =  strtoupper($rn);}
  require_once('dbconnect.php');
- $sql="SELECT * FROM vin WHERE rn = '$rn' ORDER BY timestamp DESC LIMIT 1";
+ $sql="SELECT * FROM autobook_vin WHERE rn = '$rn' ORDER BY timestamp DESC LIMIT 1";
  $result = $con -> query($sql);
  
   function prettycarnumber($auto) {
@@ -32,29 +32,25 @@ $output->values=array();
 							
 							$x->vin=$rws["vin"]; 
 							$x->registration_number=prettycarnumber($rws["rn"]);
-							$x->tel=$rws["tel"];
-							$x->email=$rws["email"];
+							
+							
 							  $data=substr($rws["rca"],8,2).'-'.substr($rws["rca"],5,2).'-'.substr($rws["rca"],0,4);
 							$x->rca=$data;
 							  $data=substr($rws["itp"],8,2).'-'.substr($rws["itp"],5,2).'-'.substr($rws["itp"],0,4);
 							
-							$x->rcat=$rws["rcat"];
+							
 							
 							$x->itp=$data;
 							  $data=substr($rws["rovigneta"],8,2).'-'.substr($rws["rovigneta"],5,2).'-'.substr($rws["rovigneta"],0,4);
 							
-							$x->itpt=$rws["itpt"];
+							
 							
 							$x->rovigneta=$data;
-							$x->rot=$rws["rot"];
+							
 							
 							$temp[]=$x;
 														
-   //set cookie 
-$cookie_name = "vin";
-$cookie_value =$x->vin;
-setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day		
-//end_set cookie vin	
+	
 
 														
 														
